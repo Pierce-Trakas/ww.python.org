15a8f60895932a93816ee0da347cd44f070937c0
Pierce Trakas <corvikid@gmail.com>
2025-07-17 11:51:24 -0400

    added more stuff

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import (Flask,
+  render_template,
+  redirect,
+  url_for,
+  session,
+  request
+)
+
+app = Flask(__name__)
+app.secret_key = "hello"
+
+# @app.route('/', methods =['GET', 'POST'])
+# def index():
+#       if request.method == 'POST':
+#     lessonchoice = request.form['Index']
+#     if lessonchoice == "":
+#       return redirect(url_for(''))
+#     elif lessonchoice == "":
+#       return redirect(url_for(''))
+#     elif lessonchoice == "":
+#       return redirect(url_for(''))
+#     elif lessonchoice == "":
+#       return redirect(url_for(''))
+#   else:
+#     return render_template('index.html')
