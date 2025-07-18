c7e87a3e0e2107e6e53a5c267cbbaa52fe4fcdc3
Pierce Trakas <corvikid@gmail.com>
2025-07-18 13:24:16 -0400

    quiz fixed

--- app.py
+++ app.py
@@ -9,14 +9,15 @@ from flask import (Flask,
 app = Flask(__name__)
 app.secret_key = "hello"
 
-points = 0
 
-@app.route('G9_math/algebra-quiz.html', methods =['GET', 'POST'])
+@app.route('/', methods =['GET', 'POST'])
 def algebraQuiz():
-    if request.method == 'POST':
+    points=0
+    if request.method=="GET":return render_template("/G9_math/algebra-quiz.html")
+    elif request.method == 'POST':
       for i in range(1, 6):
         lessonchoice = request.form[f'q{i}']
         if lessonchoice == "right":
           points += 1
-    print(points)
+      return f"<h1>{points}/5</h1>"
         
