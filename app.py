2f013a58ad4605cc21a4e30b604c9ba9be9bdfa0
Pierce Trakas <corvikid@gmail.com>
2025-07-18 13:33:08 -0400

    quiz ACTUALLY FINALLY FIXED

--- app.py
+++ app.py
@@ -13,11 +13,12 @@ app.secret_key = "hello"
 @app.route('/', methods =['GET', 'POST'])
 def algebraQuiz():
     points=0
-    if request.method=="GET":return render_template("/G9_math/algebra-quiz.html")
+    if request.method=="GET":return render_template("/G9_math/algebra-quiz.html", points=0)
     elif request.method == 'POST':
       for i in range(1, 6):
         lessonchoice = request.form[f'q{i}']
         if lessonchoice == "right":
           points += 1
-      return f"<h1>{points}/5</h1>"
+      # return f"<h1>{points}/5</h1>"
+      return render_template("/G9_math/algebra-quiz.html", points = points)
         
